dc51ae5623bc8c4250ee3e13ac7e95c1fad03061
aaron grunwald <azgrunwald@gmail.com>
2026-06-17 14:33:42 -0400

    homework48

---
+++ cs/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework48 {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] colors = ["red", "green", "blue"];
+            string[] patterns = ["striped", "checked", "plain"];
+            for (int i = 0; i < colors.Length; i++) {
+                for (int j = 0; j < patterns.Length; j++) {
+                    Console.WriteLine(new Shirt(colors[i], patterns[j]));
+                }
+            }
+        }
+    }
+}
---
+++ cs/Shirt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework48 {
+    internal class Shirt {
+        public string Color { get; set; }
+        public string Pattern { get; set; }
+        public Shirt(string color,string pattern) {
+            Color = color;
+            Pattern = pattern;
+        }
+        public override string ToString() {
+            return $"{Color}:{Pattern}";
+        }
+    }
+}
